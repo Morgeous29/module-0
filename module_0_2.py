399c6484f3d2174eb0f02f2cec3f31ba40c3ec90
Алексей <77805051+Morgeous29@users.noreply.github.com>
2024-10-14 22:50:19 +0300

    Add files via upload

---
+++ module_0_2.py
@@ -0,0 +1,21 @@
+# test
+print((9 ** 0.5) * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2, 2 * (2 + 2), 2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+print(float('123.456'))
+print((float('123.456')) * 10)
+print(int(((float('123.456')) * 10) % 10))
+
+# 5th program
+# Решил дополнительно вывести на экран остальные цифры числа '123.456'
+print(int(((float('123.456')) * 0.01) % 10))
+print(int(((float('123.456')) * 0.1) % 10))
+print(int((float('123.456')) % 10))
+print(int(((float('123.456')) * 100) % 10))
+print(int(((float('123.456')) * 1000) % 10))
